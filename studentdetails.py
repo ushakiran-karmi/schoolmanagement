63eea58cfb6fb508b0a1d612e0ba1ad8ba851b2a
collegemanagement <ushakirankarmankar@gmail.com>
2024-03-10 15:00:24 +0530

    studentdetails are added

---
+++ studentdetails.py
@@ -0,0 +1,15 @@
+#TO GET STUDENT DETAILS
+
+def main():
+    print("\t1)TO GET THE STUDENT DETAILS")
+    print("\t2)TO ADD NEW STUDENT DETAILS")
+    print("\t3)TO REMOVE STUDENT DETAILS")
+    print("\t4)TO UPDATE STUDENT DETAILS")
+    print("\t5)TO UPDATE STUDENT NAME")
+    print("\t6)TO GET NUMBER OF STUDENT IN SCHOOL")
+    print("\t7) TO GET ALL STUDENT DETAILS")
+    print("\t8)TO GET ANY CLASS STUDENT DETAILS")
+    
+    option=input("ENTER ABOVE ANY OPTION:")
+    print()
+    
